14ea88f0ef5b1ece9051f7bb3d3bed96b85e8c34
Ben Anderson <dataknut@icloud.com>
2018-04-30 11:16:34 +1200

    renamed

--- process1minGridSpyData.R
+++ process1minGridSpyData.R
@@ -1,24 +1,33 @@
 # About ----
-# Code to process NZ GREEN Grid grid spy data
+# Code to process raw NZ GREEN Grid electricity demand data as downloaded from gridSpy
+# Purpose:
+# - process & (slightly) clean  raw data
+# - save data to 1 .csv.gz file per household
+# - report data processing/file info
+
+# It won't work without the data so you will need access to it!
 
 # Libraries ----
-library(data.table)
-library(lubridate) # keep here otherwise data.table masks various functions
-library(readr)
-library(greenGridr)
+library(data.table) # for data munching
+library(lubridate) # for date munching - keep here otherwise data.table masks various functions
+library(greenGridr) # local utilities
 
 # Local parameters ----
 fpath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/_RAW DATA/GridSpyData/" # location of data
 #fpath <- "~/Data/NZGreenGrid/gridspy/1min_orig/" # location of data
-pattern <- "rf_25*at1.csv$" # e.g. *at1.csv$ filters only 1 min data
+pattern <- "*at1.csv$" # e.g. *at1.csv$ filters only 1 min data
 
-outPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/Clean_data/gridSpy/1min/" # place to save them
+outPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/Clean_data/gridSpy/" # place to save them
 #outPath <- "~/Data/NZGreenGrid/gridspy/consolidated/"
 
-dataThreshold <- 3000 # assume any files smaller than this (bytes) = no data
+dataThreshold <- 3000 # assume any files smaller than this (bytes) = no data or some mangled xml/html. Really, we should check the contents of each file.
 
 # Code: ----
-# > Get the full 1 minute file listing ----
+# > Get the file listing ----
+
+# First check if the file list exists and it was created today
+if()
+  
 # Use the path and pattern to specify
 print(paste0("Looking for files matching ", pattern, " in ", fpath))
 filesDT <- as.data.table(list.files(path = fpath, pattern = pattern, # use the pattern to filter e.g. 1m from 30s files
@@ -74,7 +83,7 @@ for(hh in hhIDs){
   years <- unique(tempHhDT$year)
   for(m in months){
     for(y in years){
-      ofile <- paste0(outPath, hh,"_", y, "_", m, "_all_1min_data.csv")
+      ofile <- paste0(outPath, "1min/", hh,"_", y, "_", m, "_all_1min_data.csv")
       write_csv(tempHhDT[month == m & year == y], ofile)
       print(paste0("Saved ", ofile))
       cmd <- paste0("gzip -f ", "'", ofile, "'") # gzip it - use quotes in case of spaces in file name
