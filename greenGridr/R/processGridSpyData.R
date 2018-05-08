510abc5e688bcd561e39c779833a9ce584e0033e
Ben Anderson <dataknut@icloud.com>
2018-05-08 16:39:38 +1200

    amended code to match function that works; amended documentation

--- greenGridr/R/processGridSpyData.R
+++ greenGridr/R/processGridSpyData.R
@@ -1,11 +1,14 @@
-#' Check date fomrats
+#' Check date formats
 #'
-#' \code{checkDates} takes character column in a data.table which is thought to be a date and tries to work out what format the date is in.
-#' Returns the best guess as a new column dateFormat in the data.table. If can't guess, sets dateFormat to 'ambiguous'.
-#' Assumes the character column is called 'date_char' - I suppose this ought to be parameterised but...
+#' \code{checkDates} takes a character column in a data.table which is thought to be a date and tries to work out what format the date is in.
+#' Returns the best guess as a new column dateFormat in the data.table. If can't guess, sets dateFormat to 'ambiguous' so you can check for yourself.
+#'
+#' The function assumes the character column is called 'date_char' - I suppose this ought to be parameterised but...
+#'
 #' Requires data.table as it uses tstrsplit
 #'
 #' @param dt the data table to use & return
+#' @importFrom  data.table tstrsplit
 #'
 #' @author Ben Anderson, \email{b.anderson@@soton.ac.uk}
 #' @export
@@ -14,33 +17,60 @@ checkDates <- function(dt) {
   # Check the date format as it could be y-m-d or d/m/y or m/d/y :-(
   dt <- dt[, c("date_char1","date_char2", "date_char3") := data.table::tstrsplit(date_char, "/")]
   # if this split failed then tstrsplit puts the dateVar in each one so we can check
+  # this assumes we never have 9-9-9 10-10-10 or 11-11-11 or 12-12-12 !
+  # would be better if data.table::tstrsplit returned an error if the split failed? We could then check for NA?
+  dt <- dt[, splitFailed := ifelse(date_char1 == date_char2 & date_char1 == date_char3, TRUE, FALSE)]
   # and then split on / instead
-  dt <- dt[date_char1 == date_char2, c("date_char1","date_char2", "date_char3") := data.table::tstrsplit(date_char, "-")] # requires data.table
+  dt <- dt[splitFailed == TRUE, c("date_char1","date_char2", "date_char3") := data.table::tstrsplit(date_char, "-")] # requires data.table
 
- dt$dateFormat <- "ambiguous"
+  dt$dateFormat <- "ambiguous" # default
+  # Days: 1-31
+  # Months: 1 - 12
+  # Years: could be 2 digit 15 - 18 or 4 digit 2015 - 2018 (+)
+  max1 <- max(as.integer(dt$date_char1))
+  #print(paste0("max1 = " , max1))
+  max2 <- max(as.integer(dt$date_char2))
+  #print(paste0("max2 = " , max2))
+  max3 <- max(as.integer(dt$date_char3))
+  #print(paste0("max3 = " , max3))
 
-  if(max(as.integer(dt$date_char3)) > 32){
-    # char 3 = year
-    if(max(as.integer(dt$date_char2)) > 12){
-      # char 2 = day
-      dt$dateFormat <- "mdy"
-    }
-    if(max(as.integer(dt$date_char1)) > 12){
-      # char 1 = month
-      dt$dateFormat <- "dmy"
-    }
-  }
-  if(max(as.integer(dt$date_char1)) > 32){
-    # char 1 = year
-    if(max(as.integer(dt$date_char2)) > 12){
-      # char 2 = day
+  if(max1 > 31){
+    # char 1 = year so default is ymd
+    dt$dateFormat <- "ymd - default (but day/month value <= 12)"
+    if(max2 > 12){
+      # char 2 = day - very unlikely
       dt$dateFormat <- "ydm"
     }
-    if(max(as.integer(dt$date_char1)) > 12){
-      # char 1 = month
-      dt$dateFormat <- "ymd"
+    if(max3 > 12){
+      # char 3 = day
+      dt$dateFormat <- "ymd - definite"
     }
   }
+  if(max2 > 31){
+    # char 2 is year - this is very unlikely
+    if(max1 > 12){
+      # char 1 = day
+      dt$dateFormat <- "dym"
+    }
+    if(max3 > 12){
+      # char 3 = day
+      dt$dateFormat <- "myd"
+    }
+  }
+  if(max3 > 31){
+    # char 3 is year so default is dmy
+    dt$dateFormat <- "dmy - default (but day/month value <= 12)"
+    if(max1 > 12){
+      # char 1 = day so char 2 = month
+      dt$dateFormat <- "dmy - definite"
+    }
+    if(max2 > 12){
+      # char 2 = day so char 1 = month
+      dt$dateFormat <- "mdy - definite"
+    }
+  }
+
+
   return(dt)
 }
 
--- greenGridr/R/utilities.R
+++ greenGridr/R/utilities.R
@@ -34,25 +34,29 @@ findParentDirectory <- function(Parent){
 
 #' Installs and loads packages
 #'
-#' \code{myRequiredPackages} checks whether the package is already installed,
-#'   installing those which are not preinstalled. All the libraries are then load.
+#' \code{loadLibraries} checks whether the package is already installed,
+#'   installing those which are not preinstalled. All the libraries are then loaded.
 #'
 #'   Especially useful when running on virtual machines where package installation
 #'   is not persistent (Like UoS sve). It will fail if the packages need to be
-#'   installed but there is no internet access
+#'   installed but there is no internet access.
+#'
+#'   NB: in R 'require' tries to load a package but throws a warning & moves on if it's not there
+#'   whereas 'library' throws an error if it can't load the package. Hence 'loadLibraries'
+#'   https://stackoverflow.com/questions/5595512/what-is-the-difference-between-require-and-library
 #' @param ... A list of packages
-#' @param repository The repository to load functions from. Defaults to "https://cran.ma.imperial.ac.uk/"
+#' @param repo The repository to load functions from. Defaults to "https://cran.rstudio.com"
 #' @importFrom  utils install.packages
 #'
 #' @author Luke Blunden, \email{lsb@@soton.ac.uk} (original)
 #' @author Michael Harper \email{m.harper@@soton.ac.uk} (revised version)
 #' @export
 #'
-myRequiredPackages <- function(..., repository = "https://cran.rstudio.com"){
+loadLibraries <- function(..., repo = "https://cran.rstudio.com"){
 
   packages <- c(...)
 
-  # Find if package isn't installed
+  # Check if package is installed
   newPackages <- packages[!(packages %in% utils::installed.packages()[,1])]
 
   # Install if required
