c9784601ee5cf43289ea579566b313ecbeebf05b
Ben Anderson <dataknut@icloud.com>
2018-05-30 14:52:13 +1200

    updated grid spy data load/filter function - works!

--- R/loadGridSpyData.R
+++ R/loadGridSpyData.R
@@ -1,25 +1,22 @@
-filterLoad <- function(f, pattern){
-  df <- readr::read_csv(f,
-                  progress = FALSE,
-                  col_types = list(col_character(), col_datetime(), col_character(), col_double()
-                                   )
-  ) # decodes .gz on the fly, requires readr
-  dt <- as.data.table(df)
-  filteredDT <- dt[circuit %like% eval(pattern)]
-  return(filteredDT)
-}
-
-#' Loads cleaned grid spy data into a data.table
+#' Loads cleaned grid spy power data for a given circuit between two dates into a data.table
 #'
-#' \code{getCleanGridSpyData} loops over a file list and uses `readr::read_csv` to load and `rbind` them.
+#' \code{getCleanGridSpyData} loops over a file list and loads each in turn using \code{readr::read_csv}. It filters each file to
+#' extract data for particular circuits between two dates and creates some derived time/date variables before
+#' using \code{rbind} to create a single data.table which is returned.
 #'
-#' Use of read_csv enables .gz files to be autoloaded and proper parsing of dateTimes.
+#' Function matches \code{circuitPattern} to extract specific circuits and selects observations between
+#'  \code{dateFrom} and \code{dateTo}. Use this to extract any circuit you want between any given dates.
 #'
-#' @param files the files to load (so you can pass a sunset if you wish)
-#' @param pattern the circuit pattern to match
+#'  \code{circuitPattern} is passed to the \code{data.table} operator \code{\%like\%} so wild cards & stuff may work. YMMV
+#'
+#'  Use of \code{readr::read_csv} enables .gz files to be autoloaded and proper parsing of dateTimes.
+#'
+#' @param files the files to load (so you can pass a subset if you wish)
+#' @param circuitPattern the circuit pattern to match
+#' @param dateFrom date to start extract (inclusive)
+#' @param dateTo date to end extract (inclusive)
 #'
 #' @import data.table
-#' @import dplyr
 #' @import readr
 #' @import hms
 #'
@@ -27,24 +24,30 @@ filterLoad <- function(f, pattern){
 #' @export
 #'
 #'
-getCleanGridSpyData <- function(files) {
+getCleanGridSpyData <- function(files, circuitPattern, dateFrom, dateTo) {
     # loop over files in list and rbind them
     # load into a single data.table
     nFiles <- length(files)
     print(paste0("# Loading ",nFiles, " files..."))
-
     # don't use parallel for file reading - no performance gain
     # http://stackoverflow.com/questions/22104858/is-it-a-good-idea-to-read-write-files-in-parallel
+    dataDT <- data.table::data.table()
+    for(f in files){# should use lapply but...
+      print(paste0("# Loading ", f))
+      df <- readr::read_csv(f,
+                            progress = FALSE,
+                            col_types = list(col_character(), col_datetime(), col_character(), col_double()
+                            )
+      ) # decodes .gz on the fly, requires readr
+      dt <- as.data.table(df)
+      # filter on circuit label pattern and dates (inclusive)
+      filteredDT <- dt[circuit %like% circuitPattern & as.Date(r_dateTime) >= dateFrom & as.Date(r_dateTime) <= dateTo]
+      print(paste0("# Found: ", tidyNum(nrow(filteredDT)), " that match -> ", circuitPattern,
+                   " <- between ", dateFrom, " and ", dateTo,
+                   " out of ", tidyNum(nrow(dt))))
+      dataDT <- rbind(dataDT, filteredDT)
+      }
 
-    df <- lapply(files, function(f)
-      readr::read_csv(f,
-               progress = FALSE,
-               col_types = list(col_character(), col_datetime(), col_character(), col_double())
-      ) # decodes .gz on the fly, requires readr https://blog.rstudio.org/2016/08/05/readr-1-0-0/
-    )  %>% dplyr::bind_rows()
-    # https://stackoverflow.com/questions/28657690/how-to-combine-result-of-lapply-to-a-data-frame
-
-    dataDT <- data.table::as.data.table(df) # convert to dt
     print("# > Setting useful dates & times")
     dataDT <- dataDT[, timeAsChar := format(r_dateTime, format = "%H:%M:%S")]
     dataDT <- dataDT[, obsHourMin := hms::as.hms(timeAsChar)] # makes graphs easier
