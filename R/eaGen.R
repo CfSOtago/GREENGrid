58dd369827dc454115e97a097f91b301e304e3a4
Ben Anderson <dataknut@icloud.com>
2018-06-15 22:33:14 +1200

    added EA embedded gen reshape function

--- R/eaGen.R
+++ R/eaGen.R
@@ -39,3 +39,13 @@ setEAGenTimePeriod <- function(dt){
   dt <- dt[, c("t","tp","mins","hours","strTime") := NULL]  #remove these now we're happy
   return(dt)
 }
+
+reshapeEAEmbeddedGenDT <- function(dt){
+  # reshape the data as it comes in a rather unhelpful form
+  reshapedDT <- melt(dt,
+                     id.vars=c("POC","NWK_Code", "Participant_Code", "Loss_Code", "Flow_Direction","Trading_date"),
+                     variable.name = "Time_Period", # converts TP1-48/49/50 <- beware of these ref DST!
+                     value.name = "kWh" # energy - see https://www.emi.ea.govt.nz/Wholesale/Datasets/Generation/Generation_MD/
+  )
+  return(reshapedDT)
+}
