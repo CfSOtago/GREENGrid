2e5202fd93e634fcf9b330906cd06aad577e945b
Ben Anderson <dataknut@icloud.com>
2018-07-30 13:06:10 +1200

    amended

--- analysis/ggDemandProfiles/howToReadExtractedFiles.R
+++ analysis/ggDemandProfiles/howToReadExtractedFiles.R
@@ -13,18 +13,4 @@ summary(df)
 # compare:
 df <- readr::read_csv("~/Data/NZGreenGrid/safe/gridSpy/1min/dataExtracts/Heat Pump_2015-04-01_2016-03-31_observations.csv.gz")
 
-
-
-df <- read_csv("/Volumes/hum-csafe/Research Projects/GREEN Grid/_RAW DATA/Aurora_CPD/Edited_whole.csv",
-               col_types =  cols(
-    DateTime = col_character(),
-    month = col_integer(),
-    CPDmin12 = col_double(),
-    CPDmin13 = col_double(),
-    CPDmin14 = col_double(),
-    CPDmin15 = col_double(),
-    CPDmin16 = col_double()
-  )
-)
-
 summary(df)
